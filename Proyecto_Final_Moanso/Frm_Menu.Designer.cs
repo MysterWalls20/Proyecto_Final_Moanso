947cb01ef9f62c34e3ac32a060df6f6a97ac9368
Carlos Paredes <wallscept20@gmail.com>
2025-06-02 22:30:51 -0500

    Datos recuperado

---
+++ Proyecto_Final_Moanso/Frm_Menu.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Proyecto_Final_Moanso
+{
+    partial class Frm_Menu
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Menu));
+            this.PanelMenu = new System.Windows.Forms.Panel();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btnAlmacen = new System.Windows.Forms.Button();
+            this.btnProducto = new System.Windows.Forms.Button();
+            this.btnInventario = new System.Windows.Forms.Button();
+            this.btnOrden = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PanelMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PanelMenu
+            // 
+            this.PanelMenu.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.PanelMenu.Controls.Add(this.btnSalir);
+            this.PanelMenu.Controls.Add(this.btnAlmacen);
+            this.PanelMenu.Controls.Add(this.btnProducto);
+            this.PanelMenu.Controls.Add(this.btnInventario);
+            this.PanelMenu.Controls.Add(this.btnOrden);
+            this.PanelMenu.Controls.Add(this.pictureBox1);
+            this.PanelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PanelMenu.Location = new System.Drawing.Point(0, 0);
+            this.PanelMenu.Name = "PanelMenu";
+            this.PanelMenu.Size = new System.Drawing.Size(254, 560);
+            this.PanelMenu.TabIndex = 0;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSalir.ImageKey = "exit_closethesession_close_6317.png";
+            this.btnSalir.ImageList = this.imageList1;
+            this.btnSalir.Location = new System.Drawing.Point(92, 487);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(61, 61);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.UseVisualStyleBackColor = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "exit_closethesession_close_6317.png");
+            // 
+            // btnAlmacen
+            // 
+            this.btnAlmacen.Location = new System.Drawing.Point(0, 378);
+            this.btnAlmacen.Name = "btnAlmacen";
+            this.btnAlmacen.Size = new System.Drawing.Size(254, 61);
+            this.btnAlmacen.TabIndex = 3;
+            this.btnAlmacen.Text = "Almacen";
+            this.btnAlmacen.UseVisualStyleBackColor = true;
+            // 
+            // btnProducto
+            // 
+            this.btnProducto.Location = new System.Drawing.Point(0, 320);
+            this.btnProducto.Name = "btnProducto";
+            this.btnProducto.Size = new System.Drawing.Size(254, 61);
+            this.btnProducto.TabIndex = 3;
+            this.btnProducto.Text = "Producto";
+            this.btnProducto.UseVisualStyleBackColor = true;
+            // 
+            // btnInventario
+            // 
+            this.btnInventario.Location = new System.Drawing.Point(0, 262);
+            this.btnInventario.Name = "btnInventario";
+            this.btnInventario.Size = new System.Drawing.Size(254, 61);
+            this.btnInventario.TabIndex = 2;
+            this.btnInventario.Text = "Entrada/Salida";
+            this.btnInventario.UseVisualStyleBackColor = true;
+            // 
+            // btnOrden
+            // 
+            this.btnOrden.Location = new System.Drawing.Point(0, 203);
+            this.btnOrden.Name = "btnOrden";
+            this.btnOrden.Size = new System.Drawing.Size(254, 61);
+            this.btnOrden.TabIndex = 1;
+            this.btnOrden.Text = "Orden de compra";
+            this.btnOrden.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Proyecto_Final_Moanso.Properties.Resources.Corporacion_Rubio;
+            this.pictureBox1.Location = new System.Drawing.Point(48, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(154, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(254, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(622, 560);
+            this.panel1.TabIndex = 1;
+            // 
+            // Frm_Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 560);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.PanelMenu);
+            this.Name = "Frm_Menu";
+            this.Text = "Menu";
+            this.PanelMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelMenu;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnAlmacen;
+        private System.Windows.Forms.Button btnProducto;
+        private System.Windows.Forms.Button btnInventario;
+        private System.Windows.Forms.Button btnOrden;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
